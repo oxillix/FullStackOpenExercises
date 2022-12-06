cf7d00c1b408b37de8a960ffd91d98654da0c373
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2022-12-06 16:43:05 +0100

    part 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,6 +11,32 @@ const Content = props => {
   )
 }
 
+const Statistics = (props) => {
+  const feedback = props.feedback
+
+  const good = feedback[0]
+  const neutral = feedback[1]
+  const bad = feedback[2]
+
+  const sumArray = (arr) => arr.reduce((partialSum, a) => partialSum + a, 0)
+  const avgArray = (arr) => (arr[0] - arr[2]) / sumArray(arr)
+  const positiveInArray = (arr) => (arr[0] / sumArray(arr)) * 100 + " %"
+
+  return (
+    <div>
+      <Header value="statistics" />
+
+      <Content text="good" value={good} />
+      <Content text="neutral" value={neutral} />
+      <Content text="bad" value={bad} />
+
+      <Content text="all" value={sumArray(feedback)} />
+      <Content text="average" value={avgArray(feedback)} />
+      <Content text="positive" value={positiveInArray(feedback)} />
+    </div>
+  )
+}
+
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
@@ -23,10 +49,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const sumArray = (arr) => arr.reduce((partialSum, a) => partialSum + a, 0)
-  const avgArray = (arr) => (arr[0] - arr[2]) / sumArray(arr)
-  const positiveInArray = (arr) => (arr[0] / sumArray(arr)) * 100 + " %"
-
   return (
     <div>
       <Header value="give feedback" />
@@ -34,15 +56,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
-      <Header value="statistics" />
-      <Content text="good" value={good} />
-      <Content text="neutral" value={neutral} />
-      <Content text="bad" value={bad} />
-
-      <Content text="all" value={sumArray([good, neutral, bad])} />
-      <Content text="average" value={avgArray([good, neutral, bad])} />
-      <Content text="positive" value={positiveInArray([good, neutral, bad])} />
-
+      <Statistics feedback={[good, neutral, bad]} />
     </div>
   )
 }
