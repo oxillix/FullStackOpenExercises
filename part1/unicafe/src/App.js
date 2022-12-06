1eb0eb70ede029aef3d2f29d591b6aef63cbbcab
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2022-12-06 16:29:52 +0100

    part 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,14 @@ import { useState } from 'react'
 
 
 const Header = props => <h1>{props.value}</h1>
-const Content = props => <p>{props.text} {props.value}</p>
+const Content = props => {
+  return (
+    <>
+      <span>{props.text} {props.value}</span>
+      <br/>
+    </>
+  )
+}
 
 const Button = (props) => (
   <button onClick={props.handleClick}>
@@ -16,6 +23,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const sumArray = (arr) => arr.reduce((partialSum, a) => partialSum + a, 0)
+  const avgArray = (arr) => (arr[0] - arr[2]) / sumArray(arr)
+  const positiveInArray = (arr) => (arr[0] / sumArray(arr)) * 100 + " %"
+
   return (
     <div>
       <Header value="give feedback" />
@@ -27,6 +38,10 @@ const App = () => {
       <Content text="good" value={good} />
       <Content text="neutral" value={neutral} />
       <Content text="bad" value={bad} />
+
+      <Content text="all" value={sumArray([good, neutral, bad])} />
+      <Content text="average" value={avgArray([good, neutral, bad])} />
+      <Content text="positive" value={positiveInArray([good, neutral, bad])} />
 
     </div>
   )
