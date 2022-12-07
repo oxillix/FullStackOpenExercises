dadd2c88085daa62c7621864358377cc5d91aa5d
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2022-12-07 21:11:33 +0100

    2.1: Course information step6

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,14 @@
+import Content from "./Content";
+import Header from "./Header";
+
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
