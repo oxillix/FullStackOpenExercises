4240959b1fb1cc31b436ab82732175e9e015840e
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2022-12-31 15:08:19 +0100

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AddPersonForm from "./components/AddPersonForm";
-import Contact from "./components/Contact";
 import FilterPerson from "./components/FilterPerson";
 import Persons from "./components/Persons";
 
