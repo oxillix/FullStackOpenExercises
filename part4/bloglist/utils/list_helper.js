e19cdb7c5ded2499f5983bb2808ab2e7512c73ec
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-01-09 22:01:20 +0100

    4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
-  return 1
-}
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs
+        .map((blog) => blog.likes)
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+};
 
 module.exports = {
-  dummy
-}
+  dummy,
+  totalLikes,
+};
