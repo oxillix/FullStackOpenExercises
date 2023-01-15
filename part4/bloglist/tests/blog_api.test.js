acdf64cdef5873b7af1a09573a53769cc39c5323
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-01-15 22:08:32 +0100

    4.8: Blog list tests, step1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+const Blog = require("../models/blog");
+const helper = require("./test_helper");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initalBlogs);
+});
+
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("all blogs are returned", async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(helper.initalBlogs.length);
+});
+
+test("a specific blog is within the returned blogs", async () => {
+  const response = await api.get("/api/blogs");
+
+  const titles = response.body.map((r) => r.title);
+
+  expect(titles).toContain("React patterns");
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
